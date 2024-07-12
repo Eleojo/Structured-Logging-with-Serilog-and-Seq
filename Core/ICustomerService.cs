9e8dafac4162db0a1f1657c757ca443c7945fb44
Eleojo <adegbeeleojo@gmail.com>
2024-07-12 22:46:53 +0100

    Add project files.

---
+++ Core/ICustomerService.cs
@@ -0,0 +1,21 @@
+﻿using Data.Dto;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public interface ICustomerService
+    {
+        Task SignUpCustomerAsync(CustomerDto customerDto);
+        Task<bool> EmailExists(string email);
+        Task<List<Customer>> GetAllCustomersAsync();
+        Task<Customer> CustomerDetails(Guid customerId);
+        Task<bool> HardDeleteCustomerAsync(Guid customerId);
+        Task<Customer> SoftDeleteCustomerAsync(Guid customerId);
+        Task<Customer> UpdateCustomerAsync(Guid customerId, string name, string email, string password);
+    }
+}
